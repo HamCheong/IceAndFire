ec52f0dc27f7746a691693040e1a199a5e21b488
HobakVine <cil1216@naver.com>
2025-07-25 21:46:07 +0900

    문 닿았을때 페이드 효과 추가

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+    public Image fadeImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("CoFadeIn");
+    }
+    IEnumerator CoFadeIn()
+    {
+        Color fadeCol = fadeImage.color; // 컬러 따오기
+        while (fadeCol.a < 1f) // 완전이 불투명해질때까지
+        {
+            fadeCol.a += 0.05f; // 조금씩 불투명해짐
+            fadeImage.color = fadeCol; //색 덮어씌우기
+            yield return new WaitForSeconds(0.05f); // 0.01초간 대기
+        }
+        SceneManager.LoadScene("Game 2"); // 다른 씬으로 이동
+    }
+
+}
--- Assets/Scripts/door.cs
+++ Assets/Scripts/door.cs
@@ -8,38 +8,22 @@ public class door : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip doorSound;
     public AudioClip leverSound;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-    }
-
+    public GameObject fadePanel;
     void OnCollisionEnter2D(Collision2D collider)
     {
         if (collider.gameObject.name == "door")
         {
             audioSource.clip = doorSound;
             audioSource.Play();
-            StartCoroutine("WaitDoorSound");
+            fadePanel.SetActive(true);
+        
         }
         if (collider.gameObject.name == "레버")
         {
             audioSource.clip = leverSound;
             audioSource.Play();
         }
-    }
-
-    IEnumerator WaitDoorSound()
-    {
-        yield return new WaitForSeconds(1f);
-        SceneManager.LoadScene("Game 2");
     }
 
 
