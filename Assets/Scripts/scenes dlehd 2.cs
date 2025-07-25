0374c982a52dab3945d637a5ebec202191c02cd0
seokhyun1111 <2025213@sungil-i.kr>
2025-07-26 00:35:43 +0900

    07-26

---
+++ Assets/Scripts/scenes dlehd 2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class scenesdlehd2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     public void sc()
+    {
+    
+ 
+        SceneManager.LoadScene("title"); // 원하는 씬으로 이동
+
+    }
+}
